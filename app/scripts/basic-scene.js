d73d2d12e76fb6032ed9bdebdd0dc43fcb523c6b
Jorge Yau <jyau01@citymail.cuny.edu>
2014-06-06 23:45:26 -0400

    Adding animation binding to cube

--- app/scripts/basic-scene.js
+++ app/scripts/basic-scene.js
@@ -1,8 +1,10 @@
 /*-------JSHint Directives-------*/
 /* global THREE                  */
+/* global requestAnimationFrame  */
 /*-------------------------------*/
 'use strict';
 
+// Create new canvas scene
 var scene  = new THREE.Scene();
 var canvasWidth  = window.innerWidth;
 var canvasHeight = window.innerHeight;
@@ -14,3 +16,18 @@ var camera = new THREE.PerspectiveCamera(75, canvasWidth/canvasHeight, 0.1, 1000
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(canvasWidth, canvasHeight);
 document.body.appendChild(renderer.domElement);
+
+// Create 3D cube
+var geometry = new THREE.CubeGeometry(1, 1, 1);
+var material = new THREE.MeshBasicMaterial(0x23a454);
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+camera.position.z = 5;
+
+
+// Render animation
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+animate();
