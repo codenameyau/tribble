c01e8ba3a3b5b39067c8ff40743bf729fccc492e
Jorge Yau <jyau01@citymail.cuny.edu>
2014-06-10 08:27:05 -0400

    Reorganizing code for pillars

--- app/scripts/basic-architecture.js
+++ app/scripts/basic-architecture.js
@@ -10,10 +10,32 @@ var containerID = '#canvas-body';
 var scene, camera, keyboard, renderer;
 var rotationSpeed = 0.02;
 var zoomX = 0;
-var zoomY = 50;
+var zoomY = 200;
 var zoomZ = 0;
 
-// Initialization
+/********************
+ * Helper Functions *
+ ********************/
+var addPillars = function(detail, pillarMaterial) {
+  var totalPillars = detail.count;
+  var radius = detail.radius || 10;
+  var height = detail.height || 50;
+  var xOffset = detail.xOffset || 10;
+  var xChange = detail.xChange || 10;
+  var cylinder = new THREE.CylinderGeometry(radius, radius, height, 32);
+
+  for (var i = 0; i < totalPillars; i++) {
+    var pillar = new THREE.Mesh(cylinder, pillarMaterial);
+    pillar.rotation.x = calc.radToDeg(90);
+    pillar.position.x = xOffset;
+    scene.add(pillar);
+    xOffset += xChange;
+  }
+};
+
+/******************
+ * Initialization *
+ ******************/
 function initScene() {
 
   // Create scene
@@ -38,7 +60,7 @@ function initScene() {
   $(containerID).append(renderer.domElement);
 
   // Example grid stage
-  var lines = 20, step = 2;
+  var lines = 80, step = 4;
   var floorGrid = new THREE.Geometry();
   var gridLine = new THREE.LineBasicMaterial({color: 'white'});
   for (var i = -lines; i <= lines; i += step) {
@@ -50,28 +72,14 @@ function initScene() {
   var stage = new THREE.Line(floorGrid, gridLine, THREE.LinePieces);
   scene.add(stage);
 
-}
-
-// Add pillars to scene
-function addPillars(detail, pillarMaterial) {
-  xOffset = detail.xOffset || 10;
-  xChange = detail.xChange || 10;
-  var xOffset, yOffset, xChange, yChange, totalPillars;
-  var cylinder = new THREE.CylinderGeometry(10, 10, 60, 32)
-  for (var i = 0; i < totalPillars; i++) {
-    var pillar = new THREE.Mesh(cylinder, pillarMaterial);
-    pillar.rotation.x = calc.radToDeg(90);
-    pillar.position.x = xOffset;
-    pillar.position.y = yOffset;
-    scene.add(pillar);
-    xOffset += xChange;
-    yOffset += yChange;
-  }
+  // Add pillars
+  var pillarDetails = {count: 8, radius: 2, height: 50};
+  var pillarMaterial = new THREE.LineBasicMaterial({color: 0xFFFFFF});
+  addPillars(pillarDetails, pillarMaterial);
 }
 
 // Keyboard event listener
 function updateKeyboard() {
-
   var x = camera.position.x;
   var y = camera.position.y;
   var z = camera.position.z;
