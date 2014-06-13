8151a145ae9a01c9ca046a161d93c1eb0d6afc73
Jorge Yau <jyau01@citymail.cuny.edu>
2014-06-13 16:33:36 -0400

    Removing gulp jshint, adding threejs plugins

--- gulpfile.js
+++ gulpfile.js
@@ -16,8 +16,6 @@ gulp.task('styles', function () {
 
 gulp.task('scripts', function () {
     return gulp.src('app/scripts/**/*.js')
-        .pipe($.jshint())
-        .pipe($.jshint.reporter(require('jshint-stylish')))
         .pipe($.size());
 });
 
