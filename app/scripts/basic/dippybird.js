2fa09b5c0dce0f440165cddd8d375349cb54da30
Jorge Yau <jyau01@citymail.cuny.edu>
2014-07-10 00:21:40 -0400

    Adding bird head, hat, face

--- app/scripts/basic/dippybird.js
+++ app/scripts/basic/dippybird.js
@@ -16,8 +16,8 @@ var CAMERA = {
   near : 1,
   far : 2000,
   zoomX : 0,
-  zoomY : 80,
-  zoomZ : 80,
+  zoomY : 150,
+  zoomZ : 100,
 };
 
 // OrbitControls settings
@@ -59,13 +59,55 @@ function basicFloorGrid(lines, steps, gridColor) {
 }
 
 function dippyBird() {
+  // Bird parameters
   var figure = new THREE.Object3D();
-  // Bird Body
-  var sphere = new THREE.SphereGeometry( 10, 32, 32);
-  var headMaterial = new THREE.MeshLambertMaterial({color: 0xDD3333, transparent: true});
-  var figureHead = new THREE.Mesh(sphere, headMaterial);
-  figure.add(figureHead);
+  var bodyHeight = 80;
+  var headRadius = 4;
 
+  // Define bird head
+  var figureHead = new THREE.Object3D();
+  var headMaterial = new THREE.MeshLambertMaterial({color: 0xEE1111, transparent: true, opacity: 0.85});
+  var eyeMaterial = new THREE.MeshLambertMaterial({color: 0xEEEEEE});
+  var irisMaterial = new THREE.MeshLambertMaterial({color: 0x111111});
+  var headGeometry = new THREE.SphereGeometry(headRadius, 32, 32);
+  var noseGeometry = new THREE.CylinderGeometry(1, 0.3, 5, 32);
+  var eyeGeometry = new THREE.CircleGeometry(0.8, 16);
+  var irisGeometry = new THREE.CircleGeometry(0.4, 16);
+  var birdHead = new THREE.Mesh(headGeometry, headMaterial);
+  var birdNose = new THREE.Mesh(noseGeometry, headMaterial);
+  var leftEye = new THREE.Mesh(eyeGeometry, eyeMaterial);
+  var rightEye = leftEye.clone();
+  var leftIris = new THREE.Mesh(irisGeometry, irisMaterial);
+  var rightIris = leftIris.clone();
+
+  // Set parameters of head
+  birdNose.position.set(0, 0, 6);
+  birdNose.rotation.x = degToRad(-90);
+  leftEye.position.set(-1.8, 2, 3.5);
+  rightEye.position.set(1.8, 2, 3.5);
+  leftIris.position.set(-1.8, 2, 3.55);
+  rightIris.position.set(1.8, 2, 3.55);
+  figureHead.add(birdHead);
+  figureHead.add(birdNose);
+  figureHead.add(leftEye);
+  figureHead.add(rightEye);
+  figureHead.add(leftIris);
+  figureHead.add(rightIris);
+
+  // Bird hat
+  var hatMaterial = new THREE.MeshPhongMaterial({color: 0x1111EE});
+  var hatBaseGeometry = new THREE.CylinderGeometry(headRadius+1.5, headRadius+1.5, 0.5, 32);
+  var hatTopGeometry = new THREE.CylinderGeometry(headRadius-1, headRadius-0.5, 5, 32);
+  var hatBase = new THREE.Mesh(hatBaseGeometry, hatMaterial);
+  var hatTop = new THREE.Mesh(hatTopGeometry, hatMaterial);
+  hatBase.position.set(0, headRadius-0.5, 0);
+  hatTop.position.set(0, headRadius+2, 0);
+  figureHead.add(hatBase);
+  figureHead.add(hatTop);
+
+  // Configure figure
+  figure.add(figureHead);
+  figure.position.set(0, bodyHeight/2, 0);
   return figure;
 }
 
@@ -117,14 +159,15 @@ function initializeScene() {
   $(containerID).append(renderer.domElement);
 
   // Light sources
-  var lightAmbient = new THREE.AmbientLight(0x5a5a5a);
-  var lightSource = new THREE.PointLight(0x7a7a7a);
-  lightSource.position.set(0, 50, 80);
-  scene.add(lightAmbient);
-  scene.add(lightSource);
+  var frontLight = new THREE.DirectionalLight(0xaaaaaa);
+  var sideLight = new THREE.DirectionalLight(0xaaaaaa);
+  frontLight.position.set(0, 0.5, 0.5);
+  sideLight.position.set(0.5, 0.8, 0);
+  scene.add(frontLight);
+  scene.add(sideLight);
 
   // Starter floor grid
-  scene.add(basicFloorGrid(80, 5));
+  scene.add(basicFloorGrid(80, 8));
 
   // Dippy bird
   scene.add(dippyBird());
