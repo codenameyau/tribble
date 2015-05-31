0e8572c1c499dfbed31f1872b9d72d1b91e44d15
Jorge Yau <jyau01@citymail.cuny.edu>
2015-05-31 11:28:02 -0400

    #18 - Services for projectSrv

--- public/app/scripts/tribble.js
+++ public/app/scripts/tribble.js
@@ -13,30 +13,55 @@ app.config(['$stateProvider', function($stateProvider) {
     templateUrl: 'templates/home.html'
   });
 
+  $stateProvider.state('project', {
+    url: '/project/:slug',
+    controller: 'ProjectCtrl as project',
+    templateUrl: 'templates/project.html'
+  });
+
 }]);
 
 
 /********************************************************************
 * SERVICES
 *********************************************************************/
-app.service('projects', ['$http', function ($http) {
-  this.getProjects = function() {
-    return $http.get('app/data/projects.json').then(function(res) {
+app.service('projectSrv', ['$http', function ($http) {
+  this.projects = $http.get('app/data/projects.json', {cache: true})
+    .then(function(res) {
       return res.data;
     });
+
+  this.getProjects = function(cb) {
+    return this.projects.then(function(data) {
+      cb(data);
+    });
   };
+
+  this.getProjectsBySlug = function(slug, cb) {
+    return this.projects.then(function(data) {
+      cb(data.filter(function(obj) {
+        return obj.slug === slug;
+      }));
+    });
+  };
+
 }]);
 
 
 /********************************************************************
 * CONTROLLERS
 *********************************************************************/
-app.controller('HomeCtrl', ['$scope', 'projects',
-  function($scope, projects) {
-
-    // Bind projects list to scope.
-    projects.getProjects().then(function(data) {
+app.controller('HomeCtrl', ['$scope', 'projectSrv',
+  function($scope, projectSrv) {
+    projectSrv.getProjects(function(data) {
       $scope.projects = data;
     });
+}]);
 
+app.controller('ProjectCtrl', ['$scope', '$stateParams', 'projectSrv',
+  function($scope, $stateParams, projectSrv) {
+    var slug = $stateParams.slug;
+    projectSrv.getProjectsBySlug(slug, function(data) {
+      $scope.projects = data;
+    });
 }]);
